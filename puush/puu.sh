cb5270d8693aedabbb70b9052a2ce82e13f4b3f0
Jamie Taylor <jamie@thefrontiergroup.com.au>
2012-03-01 14:55:27 +0800

    SSL, baby!

--- puush/puu.sh
+++ puush/puu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 EMAIL=#<email>
 APIKEY=#<apikey>
-ENDPOINT=http://puush.me
+ENDPOINT=https://puush.me
 
 if [ $# -eq 1 ]; then
   curl -s -F "z=lol" -F "e=$EMAIL" -F "k=$APIKEY" -F f=@$1 $ENDPOINT/api/up/ | cut -d',' -f 2
